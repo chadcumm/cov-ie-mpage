2e35c4393eaf08e666817615edb96b3649380cb7
chadcumm <algeron@gmail.com>
2024-02-01 19:56:02 -0600

    added version and updates to base config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ie-mpage'`, () => {
+  it(`should have as title 'cov-ie-mpage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ie-mpage');
+    expect(app.title).toEqual('cov-ie-mpage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ie-mpage app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('cov-ie-mpage app is running!');
   });
 });
